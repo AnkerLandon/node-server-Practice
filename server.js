58281eb5ee30f3255f5a1573d10739a40d94d3d6
Lando <anker.landon@gmail.com>
2018-08-12 19:41:33 -0500

    Add project page

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.get('/bad', (req, res) => {
-    res.send({
-        errorMessage: 'FUCK'
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
     });
 });
 
